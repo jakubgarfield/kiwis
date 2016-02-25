1bef8fdcd7b43bd9893f5fe723d856ed8b703bc0
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-02-26 07:41:22 +1300

    Fix RSS images

--- lib/default.rb
+++ lib/default.rb
@@ -146,3 +146,11 @@ class GeolocationFilter < Nanoc::Filter
     </Placemark>"
   end
 end
+
+class AbsoluteAtom < Nanoc::Filter
+  identifier :absolute_urls
+  type :text
+  def run(content, params={})
+    content.gsub(/((href|src)=")\//, '\1' + @site.config[:base_url] + '/')
+  end
+end
