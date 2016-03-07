a0d4aa65fbe22a87f5ef9594d4c00056da6df9d1
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-03-07 16:23:06 +1300

    Add leaflet as a local resource

--- content/resources/scripts.js
+++ content/resources/scripts.js
@@ -1,1 +1,1 @@
-<%= combine_files_content ["blueimp-gallery.min.js", "default.js"] %>
+<%= combine_files_content ["blueimp-gallery.min.js", "d3.v3.min.js", "leaflet.js", "leaflet_elevation-0.0.4.js", "default.js"] %>
