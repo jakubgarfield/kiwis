499732d6ec67787e5c585d32f14ecb4fa9c0c246
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2016-03-07 16:04:14 +1300

    Add mozjpeg installed via brew

--- lib/resize_image.rb
+++ lib/resize_image.rb
@@ -8,6 +8,12 @@ class ResizeImage < Nanoc::Filter
     width = params[:width]
     puts "Converting #{filename}"
     `convert #{filename} -resize #{width}x\\> -strip -quality #{quality} #{output_filename}`
-    `/opt/mozjpeg/bin/jpegtran -outfile #{output_filename} -optimize #{output_filename}`
+    if File.exists?("/opt/mozjpeg/bin/jpegtran")
+      `/opt/mozjpeg/bin/jpegtran -outfile #{output_filename} -optimize #{output_filename}`
+    elsif File.exists?("/usr/local/opt/mozjpeg/bin/jpegtran")
+      `/usr/local/opt/mozjpeg/bin/jpegtran -outfile #{output_filename} -optimize #{output_filename}`
+    else
+      raise "Mozjpeg not installed"
+    end
   end
 end
